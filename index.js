531381f363a712d74f8e4b699dbc212d549ccdd4
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-09-14 13:50:10 +0200

    done three

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+let mockdata = [
+    { text: "Build a to do app!", status: 'open', id: '01' },
+    { text: "Learn SQL", status: 'open', id: '02'  },
+    { text: "Practice JS on Codewars", status: 'open', id: '03'   },
+    { text: "Commit your code!", status: 'open', id: '04'},
+    { text: "Relax", status: 'open', id: '05' },
+    { text: "Test the app", status: 'open', id: '06' },
+  ]
+  
+
+app.get('/todos', (req, res) => {
+  res.status(200).json(mockdata);
+});
+
+app.post('/todos', (req, res) => {
+    res.status(201).json({ message: 'Data created successfully' });
+  });
+
+
+
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is running on port ${port}`)
 })
