f9bd900480815bd2156b1f03473bdfe04dfa1905
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-09-16 15:40:43 +0200

    done task two

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const app = express();
 const port = 3000;
 
 app.use(express.json()); 
 
 let mockdata = [
-  { text: "Build a to-do app!", status: 'open', id: '01' },
-  { text: "Learn SQL", status: 'open', id: '02' },
-  { text: "Practice JS on Codewars", status: 'open', id: '03' },
-  { text: "Commit your code!", status: 'open', id: '04' },
-  { text: "Relax", status: 'open', id: '05' },
-  { text: "Test the app", status: 'open', id: '06' },
+  { task: "Build a to-do app!", status: 'open', id: '01' },
+  { task: "Learn SQL", status: 'open', id: '02' },
+  { task: "Practice JS on Codewars", status: 'open', id: '03' },
+  { task: "Commit your code!", status: 'open', id: '04' },
+  { task: "Relax", status: 'open', id: '05' },
+  { task: "Test the app", status: 'open', id: '06' },
 ];
+
+// validation and sanitization rules for a valid task
+
+const validTask = [
+  body('task')
+  .notEmpty()
+  .withMessage('Task cannot be empty')
+  .trim()
+  .escape(),
+  body('status')
+  .isIn(['open','done'])
+  .withMessage('Status can only be "open" or "done"')
+  .trim()
+  .escape(),
+  body('id')
+  .isUUID()
+  .withMessage('Id needs to be a UUID')
+  .trim()
+  .escape(),
+
+]
 
 app.get('/todos', (req, res) => {
   res.status(200).json(mockdata);
 });
 
-app.post('/todos', (req, res) => {
-  const newTask = req.body;
+// GET request for /todos/:id
 
+app.post('/todos',validTask, (req, res) => {
+  const errors = validationResult(req);
+
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array() });
+  }
+  const newTask = req.body;
   mockdata.push(newTask);
 
   res.status(201).json({ message: 'Data created successfully' });
 });
+
+
 
 // PUT request for /todos
 app.put('/todos', (req, res) => { 
@@ -36,14 +67,13 @@ app.delete('/todos', (req, res) => {
   res.status(200).json({ message: 'Data deleted successfully' });
 });
 
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Path not found' });
-});
+// Error handling
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Internal server error' });
+  console.error(err);
+  res.status( err.statusCode || 500).json({ message: 'Internal server error' });
 });
+
 
 
 
