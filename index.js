1f23d5d40367e9bfe75e10141fb7c0f31fa64724
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-09-16 21:16:12 +0200

    donethree

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body, validationResult } = require('express-validator');
+const { body, param, validationResult } = require('express-validator');
 const { v4: uuidv4 } = require('uuid'); 
 const app = express();
 const port = 3000;
@@ -23,11 +23,13 @@ const validTask = [
   .withMessage('Task cannot be empty')
   .trim()
   .escape(),
+
   body('status')
   .isIn(['open','done'])
   .withMessage('Status can only be "open" or "done"')
   .trim()
   .escape(),
+
   body('id')
   .isUUID()
   .withMessage('Id needs to be a UUID')
@@ -35,6 +37,16 @@ const validTask = [
   .escape(),
 
 ]
+
+// Validation rule for a valid ID parameter
+const validIdParam = [
+  param('id')
+    .isUUID()
+    .withMessage('Id parameter needs to be a UUID')
+    .trim()
+    .escape(),
+];
+
 
 app.get('/todos', (req, res) => {
   res.status(200).json(mockdata);
@@ -48,31 +60,52 @@ console.log(uuid);
 
 // GET request for /todos/:id
 
-app.post('/todos',validTask, (req, res) => {
+app.post('/todos', validTask, (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array() });
+    return res.status(400).json({ errors: errors.array() });
   }
 
-
   const newTask = {
-
     task: req.body.task,
     status: req.body.status,
     id: uuidv4(),
-  }
+  };
+
   mockdata.push(newTask);
 
   res.status(201).json({ message: 'Data created successfully' });
 });
 
 
+//// ask Nathali 
+
+
 
 // PUT request for /todos
-app.put('/todos', (req, res) => { 
+app.put('/todos/:id', validIdParam, validTask, (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const taskId = req.params.id;
+  const updatedTask = req.body;
+
+
+  if (taskIndex === -1) {
+    return res.status(404).json({ message: 'Task not found' });
+  }
+
+  mockdata[taskIndex] = { ...mockdata[taskIndex], ...updatedTask };
+
   res.status(200).json({ message: 'Data updated successfully' });
 });
+
+
+
 
 // DELETE request for /todos
 app.delete('/todos', (req, res) => {
