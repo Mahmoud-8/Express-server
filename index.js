f458ae199a118667e62be2c39d9e2cf943886769
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-09-17 11:03:34 +0200

    done task four

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { body, param, validationResult } = require('express-validator');
-const { v4: uuidv4 } = require('uuid'); 
+const { body, validationResult } = require('express-validator');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = 3000;
 
-app.use(express.json()); 
+app.use(express.json());
 
 let mockdata = [
   { task: "Build a to-do app!", status: 'open', id: '01' },
@@ -15,51 +15,26 @@ let mockdata = [
   { task: "Test the app", status: 'open', id: '06' },
 ];
 
-// validation and sanitization rules for a valid task
-
+// Validation and sanitization rules for a valid task
 const validTask = [
   body('task')
-  .notEmpty()
-  .withMessage('Task cannot be empty')
-  .trim()
-  .escape(),
-
+    .notEmpty()
+    .withMessage('Task cannot be empty')
+    .trim()
+    .escape(),
   body('status')
-  .isIn(['open','done'])
-  .withMessage('Status can only be "open" or "done"')
-  .trim()
-  .escape(),
-
-  body('id')
-  .isUUID()
-  .withMessage('Id needs to be a UUID')
-  .trim()
-  .escape(),
-
-]
-
-// Validation rule for a valid ID parameter
-const validIdParam = [
-  param('id')
-    .isUUID()
-    .withMessage('Id parameter needs to be a UUID')
+    .isIn(['open', 'done'])
+    .withMessage('Status can only be "open" or "done"')
     .trim()
     .escape(),
 ];
 
-
+// GET request for /todos
 app.get('/todos', (req, res) => {
   res.status(200).json(mockdata);
 });
 
-
-// Generate a new UUID
-const uuid = uuidv4();
-console.log(uuid);
-
-
-// GET request for /todos/:id
-
+// POST request for /todos
 app.post('/todos', validTask, (req, res) => {
   const errors = validationResult(req);
 
@@ -70,7 +45,7 @@ app.post('/todos', validTask, (req, res) => {
   const newTask = {
     task: req.body.task,
     status: req.body.status,
-    id: uuidv4(),
+    id: uuidv4(), // Generate a new UUID for each task
   };
 
   mockdata.push(newTask);
@@ -78,57 +53,51 @@ app.post('/todos', validTask, (req, res) => {
   res.status(201).json({ message: 'Data created successfully' });
 });
 
-
-//// ask Nathali 
-
-
-
-// PUT request for /todos
-app.put('/todos/:id', validIdParam, validTask, (req, res) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
+// PUT request for /todos/:id (Update a specific task by ID)
+app.put('/todos/:id', validTask, (req, res) => {
   const taskId = req.params.id;
   const updatedTask = req.body;
 
+  const taskIndex = mockdata.findIndex((task) => task.id === taskId);
 
   if (taskIndex === -1) {
     return res.status(404).json({ message: 'Task not found' });
   }
 
-  mockdata[taskIndex] = { ...mockdata[taskIndex], ...updatedTask };
+  mockdata[taskIndex] = {
+    ...mockdata[taskIndex],
+    ...updatedTask,
+  };
 
   res.status(200).json({ message: 'Data updated successfully' });
 });
 
+// DELETE request for /todos/:id (Delete a specific task by ID)
+app.delete('/todos/:id', (req, res) => {
+  const taskId = req.params.id;
 
+  const taskIndex = mockdata.findIndex((task) => task.id === taskId);
 
+  if (taskIndex === -1) {
+    return res.status(404).json({ message: 'Task not found' });
+  }
 
-// DELETE request for /todos
-app.delete('/todos', (req, res) => {
+  mockdata.splice(taskIndex, 1);
 
   res.status(200).json({ message: 'Data deleted successfully' });
 });
 
 // Error handling
-
 app.use('*', (req, res, next) => {
   const error = new Error('There is no path here!');
   error.statusCode = 404;
   next(error);
 });
 
-
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status( err.statusCode || 500).json({ message: 'Internal server error' });
+  res.status(err.statusCode || 500).json({ message: 'Internal server error' });
 });
-
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
