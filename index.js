8c3afaa29d453cd025fc68b58605f019f97aa674
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-09-19 16:41:31 +0200

    done

--- index.js
+++ index.js
@@ -1,108 +1,167 @@
-const express = require('express');
-const { body, validationResult } = require('express-validator');
-const { v4: uuidv4 } = require('uuid');
-const cors = require('cors'); // Import the cors library
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const { v4: uuidv4 } = require("uuid");
+const cors = require("cors"); // Import the cors library
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+
+
+
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
- // Enable CORS middleware
- app.use(cors());
+// Enable CORS middleware
+app.use(cors());
 
 app.use(express.json());
 
-let mockdata = [
-  { task: "Build a to-do app!", status: 'open', id: '01' },
-  { task: "Learn SQL", status: 'open', id: '02' },
-  { task: "Practice JS on Codewars", status: 'open', id: '03' },
-  { task: "Commit your code!", status: 'open', id: '04' },
-  { task: "Relax", status: 'open', id: '05' },
-  { task: "Test the app", status: 'open', id: '06' },
-];
+// Connect to the MongoDB database
+
+let connectionStringOfDB = process.env.DATABASE_CONNECTION;
+
+  let options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  mongoose
+  .connect(connectionStringOfDB, options) 
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
+
+
+
+  // Define the Todo schema and model
+
+ 
+  const todoSchema = new mongoose.Schema({
+    task: String,
+    status: String,
+    id: String,
+  });
+  
+  const Todo = mongoose.models.Todo || mongoose.model('Todo', todoSchema);
+
+
 
 // Validation and sanitization rules for a valid task
 const validTask = [
-  body('task')
-    .notEmpty()
-    .withMessage('Task cannot be empty')
-    .trim()
-    .escape(),
-  body('status')
-    .isIn(['open', 'done'])
+  body("task")
+  .notEmpty()
+  .withMessage("Task cannot be empty")
+  .trim()
+  .escape(),
+
+  body("status")
+    .isIn(["open", "done"])
     .withMessage('Status can only be "open" or "done"')
     .trim()
     .escape(),
 ];
 
-// GET request for /todos
-app.get('/todos', (req, res) => {
-  res.status(200).json(mockdata);
+// GET request
+app.get('/todos', async (req, res, next) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (error) {
+    console.error('Error fetching todos:', error);
+    const getError = new Error('Could not fetch data');
+    next(getError);
+  }
 });
 
-// POST request for /todos
-app.post('/todos', validTask, (req, res) => {
-  const errors = validationResult(req);
 
+// POST request
+app.post('/todos', validTask, async (req, res, next) => {
+  const errors = validationResult(req);
+console.log(errors);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const newTask = {
-    task: req.body.task,
-    status: req.body.status,
-    id: uuidv4(),
-  };
+  try {
+    const { task, status } = req.body;
 
-  mockdata.push(newTask);
+    const newTask = await Todo.create({ task, status, id: uuidv4() });
 
-  res.status(201).json({ message: 'Data created successfully' });
+
+    res.status(201).json({
+      success: true,
+      message: `Data was saved! You now have to: ${newTask.task}`,
+    });
+  } catch (error) {
+    console.error(error);
+    const postError = new Error("Could not post data");
+    next(postError);
+  }
 });
 
-// PUT request for /todos/:id (Update a specific task by ID)
-app.put('/todos/:id', validTask, (req, res) => {
+
+
+
+
+
+// PUT request 
+app.put('/todos/:id', validTask, async (req, res, next) => {
   const taskId = req.params.id;
   const updatedTask = req.body;
 
-  const taskIndex = mockdata.findIndex((task) => task.id === taskId);
+  try {
+    const task = await Todo.findOneAndUpdate({ id: taskId }, updatedTask, {
+      new: true,
+    });
 
-  if (taskIndex === -1) {
-    return res.status(404).json({ message: 'Task not found' });
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+
+    res.status(200).json({ message: 'Data updated successfully' });
+  } catch (error) {
+    console.error(error);
+    const putError = new Error('Could not update data');
+    next(putError);
   }
-
-  mockdata[taskIndex] = {
-    ...mockdata[taskIndex],
-    ...updatedTask,
-  };
-
-  res.status(200).json({ message: 'Data updated successfully' });
 });
 
-// DELETE request for /todos/:id (Delete a specific task by ID)
-app.delete('/todos/:id', (req, res) => {
+// DELETE request 
+app.delete('/todos/:id', async (req, res, next) => {
   const taskId = req.params.id;
 
-  const taskIndex = mockdata.findIndex((task) => task.id === taskId);
+  try {
+    const task = await Todo.findOneAndDelete({ id: taskId });
 
-  if (taskIndex === -1) {
-    return res.status(404).json({ message: 'Task not found' });
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+
+    res.status(200).json({ message: 'Data deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    const deleteError = new Error('Could not delete data');
+    next(deleteError);
   }
-
-  mockdata.splice(taskIndex, 1);
-
-  res.status(200).json({ message: 'Data deleted successfully' });
 });
 
+
+
+
 // Error handling
-app.use('*', (req, res, next) => {
-  const error = new Error('There is no path here!');
+app.use("*", (req, res, next) => {
+  const error = new Error("There is no path here!");
   error.statusCode = 404;
   next(error);
 });
 
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(err.statusCode || 500).json({ message: 'Internal server error' });
+  res.status(err.statusCode || 500).json({ message: "Internal server error" });
 });
 
 app.listen(port, () => {
